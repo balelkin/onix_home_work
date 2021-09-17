1443604b727c9ea0778a65d2512857808626100f
balelkin <balelkinn@gmail.com>
2021-09-17 13:57:49 +0300

    new

--- src/main.ts
+++ src/main.ts
@@ -5,16 +5,14 @@ import { AppModule } from './components/app/app.module';
 import * as cookieParser from 'cookie-parser'
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { AllExceptionsFilter } from './components/filter/exceptionFilter';
-//import chalk from 'chalk';
+
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(
     AppModule,
   );
 
-  // const errorMsg = chalk.bgKeyword('white').redBright;
-  // const successMsg = chalk.bgKeyword('green').white;
-
+  
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '../src/public', 'views'));
   app.setViewEngine('hbs');
@@ -24,7 +22,7 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new AllExceptionsFilter());
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
   console.log('Server started on port 3000');
   Logger.log('User microservice running');
   
