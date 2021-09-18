f82a3157d7adb487bd73870ae38ba0bc03be79dd
balelkin <balelkinn@gmail.com>
2021-09-18 12:46:21 +0300

    main migrate

--- src/components/app/main.ts
+++ src/components/app/main.ts
@@ -1,10 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
-import { AppModule } from './components/app/app.module';
+import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { Logger, ValidationPipe } from '@nestjs/common';
-import { AllExceptionsFilter } from './components/filter/exceptionFilter';
+import { AllExceptionsFilter } from '../filter/exceptionFilter';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
