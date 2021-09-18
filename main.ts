234a1d330ec04e43cbd8afa76e8b8433530e6408
balelkin <balelkinn@gmail.com>
2021-09-18 17:52:46 +0300

    wwwwww

--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
-import { AppModule } from './app.module';
+import { AppModule } from './src/components/app/app.module';
 import * as cookieParser from 'cookie-parser';
 import { Logger, ValidationPipe } from '@nestjs/common';
-import { AllExceptionsFilter } from '../filter/exceptionFilter';
+import { AllExceptionsFilter } from './src/components/filter/exceptionFilter';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
